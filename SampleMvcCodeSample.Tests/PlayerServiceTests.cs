61d3d2aa66e0c1d93c86f1bdff32cb256eaaec08
Graham Kilmon <grahamk@brillianceaudio.com>
2019-07-18 10:30:17 -0400

    add tests for PlayerService

---
+++ SampleMvcCodeSample.Tests/PlayerServiceTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SampleMvcCodeSample.Core.Services;
+using SampleMvcCodeSample.Core.TransformServices;
+using SampleMvcCodeSample.Data.Repositories.Interfaces;
+using SampleMvcCodeSample.Model;
+
+namespace SampleMvcCodeSample.Tests
+{
+    [TestClass]
+    public class PlayerServiceTests
+    {
+        private PlayerService _playerService;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            IPlayerRepository testRepo = new TestPlayerRepository();
+            _playerService = new PlayerService(testRepo, new PlayerTransformService());
+        }
+
+        [TestMethod]
+        public void TestGetAllPlayers()
+        {
+            List<PlayerModel> players = _playerService.GetAllPlayers();
+
+            Assert.IsTrue(players.Count > 0);
+        }
+
+        [TestMethod]
+        public void TestGetPlayer_Exist()
+        {
+            int id = 1;
+            PlayerModel player = _playerService.GetPlayer(id);
+
+            Assert.IsTrue(player.Id == id);
+        }
+
+        [TestMethod]
+        public void TestGetPlayer_DoesntExist()
+        {
+            int id = 2;
+            PlayerModel player = _playerService.GetPlayer(id);
+
+            Assert.IsFalse(player.Id == id);
+        }
+    }
+}
---
+++ SampleMvcCodeSample.Tests/TestPlayerRepository.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SampleMvcCodeSample.Data.Models;
+using SampleMvcCodeSample.Data.Repositories.Interfaces;
+
+namespace SampleMvcCodeSample.Tests
+{
+    public class TestPlayerRepository : IPlayerRepository
+    {
+        private List<Player> playerData = new List<Player>();
+
+        public TestPlayerRepository()
+        {
+            playerData.Add(new Player()
+            {
+                Age = 10,
+                BirthPlace = "test birth",
+                Classification = PlayerClassification.DesignatedPlayer,
+                FirstName = "TestFirst",
+                Height = new PlayerHeight() { Feet = 6, Inches = 0 },
+                Hometown = "test home",
+                Id = 1,
+                JerseyNumber = 10,
+                LastName = "TestLast",
+                Nickname = "TestNick",
+                Position = PlayerPosition.Forward,
+                Stats = new List<PlayerStats>() { new PlayerStats() { Assists = 0, Club = "test club", GamesPlayed = 1, GamesStarted = 1, GoalsScored = 10, Minutes = 120, RedCards = 0, Shots = 10, ShotsOnGoal = 10, Year = 2019, YellowCards = 0 } },
+                Weight = 170,
+            });
+        }
+
+        public List<Player> GetAllPlayers()
+        {
+            return playerData;
+        }
+
+        public Player GetPlayer(int id)
+        {
+            return playerData.Where(p => p.Id == id).First();
+        }
+    }
+}
